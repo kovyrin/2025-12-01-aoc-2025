8895c9b1190bbd6b58ab1b11aed29ace08ad3531
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2025-12-01 11:14:54 -0500

    Cleanup

--- day01/part2.rb
+++ day01/part2.rb
@@ -36,45 +36,14 @@ class Dial
   end
 end
 
-class BruteforceDial < Dial
-  def turn(command)
-    direction = command[0]
-    steps = command[1..].to_i
-    hit_zero = 0
-
-    sign = direction == 'R' ? 1 : -1
-    steps.times do
-      @pos += sign
-      @pos %= 100
-      hit_zero += 1 if pos.zero?
-    end
-
-    @zero_count += hit_zero
-  end
-end
-
 input_type = ENV['REAL'] ? 'real' : 'test'
 input_file = File.join(__dir__, 'data', "input-#{input_type}.txt")
 data = File.readlines(input_file).reject(&:blank?)
 
 fast_dial = Dial.new
-bruteforce_dial = BruteforceDial.new
 
 data.each do |command|
-  puts '---------------------------'
-  puts "Pos: #{fast_dial.pos}, command: #{command}"
   fast_dial.turn(command)
-  bruteforce_dial.turn(command)
-
-  puts "  - pos: f:#{fast_dial.pos} / b:#{bruteforce_dial.pos}"
-  puts "  - zeroes: f:#{fast_dial.zero_count} / b:#{bruteforce_dial.zero_count}"
-
-  raise 'ERROR! Pos misaligned' if fast_dial.pos != bruteforce_dial.pos
-  raise 'ERROR! Zeros misaligned' if fast_dial.zero_count != bruteforce_dial.zero_count
 end
 
 puts "Total zeroes: #{fast_dial.zero_count}"
-
-# REAL answer attempts:
-# 5524 - too low
-# 6386 - correct
