b4972292cda61b464078c00c4a6aef2e88e00feb
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2025-12-03 11:52:35 -0500

    Day3 part 2

--- day03/part2.rb
+++ day03/part2.rb
@@ -3,11 +3,31 @@
 
 require 'active_support/all'
 
-day = File.basename(__dir__).sub(/\Aday/, '')
-part = File.basename(__FILE__)[/part(\d+)/, 1] || '?'
+def jolts_for_bank(bank, length)
+  return 0 if length.zero?
+
+  # Slice of the bank that can contain our first battery
+  slice_for_start = 0..(bank.count - length)
+
+  # Find the biggest battery we can to serve as the first one
+  start = 0
+  slice_for_start.each do |pos|
+    start = pos if bank[pos] > bank[start]
+  end
+
+  bank[start] * (10**(length - 1)) + jolts_for_bank(bank[start + 1..], length - 1)
+end
 
 input_type = ENV['REAL'] ? 'real' : 'test'
 input_file = File.join(__dir__, 'data', "input-#{input_type}.txt")
-data = File.readlines(input_file).reject(&:blank?)
+data = File.readlines(input_file).reject(&:blank?).map { |s| s.strip.split('').map(&:to_i) }
 
-puts "Day #{day} part #{part} not implemented yet (loaded #{data.size} lines)"
+sum = 0
+
+data.each do |batteries|
+  jolts = jolts_for_bank(batteries, 12)
+  puts "  - #{batteries.join} => #{jolts}"
+  sum += jolts
+end
+
+puts "Total jolts: #{sum}"
