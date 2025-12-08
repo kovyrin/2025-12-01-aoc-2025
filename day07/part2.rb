272027ae0441941329c58645a685f0f20755ae2c
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2025-12-07 23:07:54 -0500

    day 7 part 2

--- day07/part2.rb
+++ day07/part2.rb
@@ -3,11 +3,31 @@
 
 require 'active_support/all'
 
-day = File.basename(__dir__).sub(/\Aday/, '')
-part = File.basename(__FILE__)[/part(\d+)/, 1] || '?'
-
 input_type = ENV['REAL'] ? 'real' : 'test'
 input_file = File.join(__dir__, 'data', "input-#{input_type}.txt")
-data = File.readlines(input_file).reject(&:blank?)
+map = File.readlines(input_file).reject(&:blank?)
 
-puts "Day #{day} part #{part} not implemented yet (loaded #{data.size} lines)"
+# We start with a single ray emitted from the position of the S in the first row
+rays = { map.first.index('S') => 1 }
+
+# Process all lines one by one
+1.upto(map.count - 1) do |line_idx|
+  puts "Line #{line_idx} starting with #{rays.values.sum} rays"
+  line = map[line_idx]
+
+  new_rays = Hash.new(0)
+  rays.each do |pos, count|
+    # If we hit a splitter, the ray stops and we emit two other rays
+    if line[pos] == '^'
+      new_rays[pos - 1] += count
+      new_rays[pos + 1] += count
+    else
+      # Otherwise all the rays at this position continue
+      new_rays[pos] += count
+    end
+  end
+  rays = new_rays
+  puts " - ended with #{rays.values.sum} rays"
+end
+
+puts "Total rays at the end: #{rays.values.sum}"
